8c7b390acb8f10c19ed5d061b7d5760bf242e440
Emmanuel Courreges <emmanuel.courreges@orange.com>
2023-12-06 10:30:19 +0100

    remove retry whitelist which is removed from urllib recent api

--- ts/client.py
+++ ts/client.py
@@ -100,8 +100,7 @@ class TradeStationClient():
         # Add 2 auto retries for these status codes but not on POST/PUT
         retry_strategy = Retry(
             total=1,
-            status_forcelist=[429, 500, 502, 503, 504],
-            method_whitelist=["HEAD", "GET", "OPTIONS"]
+            status_forcelist=[429, 500, 502, 503, 504]
         )
         adapter = HTTPAdapter(max_retries=retry_strategy)
         self.session.mount("https://", adapter)
